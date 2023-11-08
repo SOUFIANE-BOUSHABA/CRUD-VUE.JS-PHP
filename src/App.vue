233917f35cb9ebb73663d37777bfff07b5023965
souf139 <soufianboushaba12@gmail.com>
2023-11-08 12:41:51 +0100

    this commit is for create formulaire

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <AppForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import AppForm from './components/AppForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppForm
   }
 }
 </script>
